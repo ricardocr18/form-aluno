9125e95450a912681b66e31e8bd8a828b68bebb3
ricardocr18 <ricardocribeiro@hotmail.com>
2021-03-25 17:47:47 -0300

    Formulário de Aluno - Projeto FCamara
    Criando o formulário do cadastro do aluno para o projeto FCamara

---
+++ bootstrap/js/formulario.js
@@ -0,0 +1,18 @@
+$('#form-contato').submit(function(){
+
+alert('oi');
+
+  /* var nome = $('#nome_aluno');
+    var erro = $('.alert'); 
+
+    if (nome.val() == '') {
+        erro.removeClass('d-none');
+
+        return false;
+    }
+
+
+    return false; */
+
+
+});
